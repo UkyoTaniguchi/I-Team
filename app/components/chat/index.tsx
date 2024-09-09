b0e102f0187c34d32cbe0781e72589f7479efe58
Ukyo <ukyou20011121@gmail.com>
2024-09-09 09:56:25 +0900

    Suspenseでwrap

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Suspense, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { auth, db } from "../../firebaseConfig";
